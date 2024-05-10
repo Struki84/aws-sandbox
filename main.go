5c63c9aacaedb3bd15a916cdfa9a13a21a2a13f8
Simun Strukan <simun.strukan@gmail.com>
2024-05-10 21:34:11 +0200

    test commit

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 
 func main() {
 	fmt.Println("Go app is running and serving HTTP request...")
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	})
