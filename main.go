79a178b8cd18cc8b045d4440d8b28d1b3d40d44f
Simun Strukan <simun.strukan@gmail.com>
2024-05-10 21:17:23 +0200

    Adding logs in main file...

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("Go app is running and serving HTTP request...")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	})
